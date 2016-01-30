e127042f756bba2d350d163d2dbb7885b35e050f
ulefebvr <ulef45av@gmail.com>
2016-01-30 18:59:41 +0100

    initialisation chr in read

--- termcaps_loop.c
+++ termcaps_loop.c
@@ -32,12 +32,14 @@ char        *termcaps_loop(t_info *info)
     int                 ret;
     long                chr;
 
+    chr = 0;
     initial_postion_cursor(info->term);
     while ((ret = read(0, &chr, sizeof(chr))) > 0)
     {
         if ((chr == NL) || (chr == CTRL_D && (ret = -1) == -1))
             break;
         termcaps_handle_keyboard(info, chr);
+        chr = 0;
     }
     termcaps_save(0);
     return ((ret == -1) ? NULL : return_string(info->term->cmd));
