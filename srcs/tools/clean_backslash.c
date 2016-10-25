153d565cbdeea0361c8946c2c97243f6ff7352ca
ulefebvr <ulefebvr@student.42.fr>
2016-10-25 18:28:56 +0200

    [BUG] Parse on backslash fixed

--- srcs/tools/clean_backslash.c
+++ srcs/tools/clean_backslash.c
@@ -6,11 +6,28 @@
 /*   By: rclanget <rclanget@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/19 11:55:39 by rclanget          #+#    #+#             */
-/*   Updated: 2016/10/25 17:24:37 by ulefebvr         ###   ########.fr       */
+/*   Updated: 2016/10/25 18:27:24 by ulefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int			get_len(char *str)
+{
+	int	 i;
+	int	 j;
+
+	i = 0;
+	j = 0;
+	while (str[j++])
+	{
+		if (str[j] != '\\')
+			i++;
+		else if (str[j] == '\\' && str[j + 1] == '\\' && ++j)
+			i++;
+	}
+	return (i);
+}
 
 char *clean_backslash(char *str)
 {
@@ -18,18 +35,15 @@ char *clean_backslash(char *str)
 	int	 j;
 	char	 *str_new;
 
-	i = 0;
 	j = 0;
-	while (str[j++])
-		if (str[j] != '\\')
-			i++;
-	if ((str_new = ft_memalloc(i)))
+	i = 0;
+	if ((str_new = ft_memalloc(get_len(str))))
 	{
-		j = 0;
-		i = 0;
 		while (str[j])
 		{
 			if (str[j] != '\\')
+				str_new[i++] = str[j++];
+			else if (str[j] == '\\' && str[j + 1] == '\\' && ++j)
 				str_new[i++] = str[j++];
 			else
 				j++;
