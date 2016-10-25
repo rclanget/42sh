f1de29a8a81b3f2fea768f03ccb6211a30ea336c
ulefebvr <ulefebvr@student.42.fr>
2016-10-25 14:09:48 +0200

    [REDIR] Pipe function fixed

--- srcs/execution/operator_pipe.c
+++ srcs/execution/operator_pipe.c
@@ -6,7 +6,7 @@
 /*   By: ulefebvr <ulefebvr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/27 16:29:30 by ulefebvr          #+#    #+#             */
-/*   Updated: 2016/03/04 15:06:56 by ulefebvr         ###   ########.fr       */
+/*   Updated: 2016/10/25 14:07:34 by ulefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int                operator_pipe(t_info *info, t_tree *cmd)
+int				operator_pipe(t_info *info, t_tree *cmd)
 {
 	int			fdp[2];
 	pid_t		pid;
@@ -23,15 +23,14 @@ int                operator_pipe(t_info *info, t_tree *cmd)
 	pipe(fdp);
 	if (cmd->right && !(pid = fork()))
 	{
-		dup2(fdp[0], 0);
+		dup2(fdp[1], 1);
 		close(fdp[0]);
-		close(fdp[1]);
-		execution_motor(info, cmd->right, 0);
+		execution_motor(info, cmd->left, 0);
 		exit(0);
 	}
-	dup2(fdp[1], 1);
+	dup2(fdp[0], 0);
 	close(fdp[1]);
-	close(fdp[0]);
-	execution_motor(info, cmd->left, 1);
+	wait(0);
+	execution_motor(info, cmd->right, 1);
 	return (0);
 }
