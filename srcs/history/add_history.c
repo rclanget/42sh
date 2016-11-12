f84d897c731e0c7c00967afc1e884a9177507ab2
ulefebvr <ulefebvr@student.42.fr>
2016-11-12 15:11:33 +0100

    [QUOTE] Multiline handle

--- srcs/history/add_history.c
+++ srcs/history/add_history.c
@@ -6,13 +6,27 @@
 /*   By: ulefebvr <ulefebvr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/27 13:58:45 by ulefebvr          #+#    #+#             */
-/*   Updated: 2016/11/06 16:31:40 by ulefebvr         ###   ########.fr       */
+/*   Updated: 2016/11/12 15:10:39 by ulefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 #include "libft.h"
 #include "history.h"
+
+char				*replace_nl(char *str)
+{
+	int				i;
+
+	i = 0;
+	while (str && str[i])
+	{
+		if (str[i] == '\n')
+			str[i] = ' ';
+		++i;
+	}
+	return (str);
+}
 
 char				*add_history(t_info *info, char *cmd)
 {
@@ -22,7 +36,7 @@ char				*add_history(t_info *info, char *cmd)
 	node = NULL;
 	if (ft_strlen(cmd) && (node = ft_memalloc(sizeof(t_history))))
 	{
-		node->cmd = ft_strdup(cmd);
+		node->cmd = replace_nl(ft_strdup(cmd));
 		node->n = number++;
 		if ((node->next = info->hist))
 			node->next->prev = node;
--- srcs/termcaps_readline/termcaps_readline.c
+++ srcs/termcaps_readline/termcaps_readline.c
@@ -3,13 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   termcaps_readline.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zipo <zipo@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ulefebvr <ulefebvr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/26 00:16:21 by ulefebvr          #+#    #+#             */
-/*   Updated: 2016/11/11 13:03:25 by zipo             ###   ########.fr       */
+/*   Updated: 2016/11/12 15:07:03 by ulefebvr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "tools.h"
 #include "get_next_line.h"
 #include "command_line_termcaps.h"
 
@@ -49,6 +50,7 @@ char		*get_currentline(t_info *info, int term, char quote)
 	char	*cmd;
 	char	*tmp;
 	char	*tmp2;
+	char	*tmp3;
 
 	ft_print(!quote ? "%s" : "> ", info->term->prompt);
 	cmd = ((term) ? termcaps_loop(info) : getnextline(0, info));
@@ -56,9 +58,10 @@ char		*get_currentline(t_info *info, int term, char quote)
 	{
 		info->term->pos_c = 0;
 		tmp = get_currentline(info, term, quote);
-		tmp2 = ft_strjoin(cmd, tmp);
-		ft_free_them_all(2, &cmd, &tmp);
-		cmd = tmp2;
+		tmp2 = ft_strjoinc(cmd, '\n');
+		tmp3 = ft_strjoin(tmp2, tmp);
+		ft_free_them_all(2, &cmd, &tmp, &tmp2);
+		cmd = tmp3;
 	}
 	return cmd;
 }
