c9fab7bc72ac39306dc7b2e0b35f1d957b45fed1
Gaetan DEGUIGNE <gdeguign@e1r1p8.42.fr>
2016-11-14 17:01:19 +0100

    [norme srcs/termcaps_readline/termcaps_handle_keyboard.c]

--- srcs/termcaps_readline/termcaps_handle_keyboard.c
+++ srcs/termcaps_readline/termcaps_handle_keyboard.c
@@ -42,7 +42,7 @@ static t_key g_key_tab[] =
 	{TAB, call_autocomp}
 };
 
-static int		 add_chr(t_info *info, long chr)
+static int		add_chr(t_info *info, long chr)
 {
 	int cmd_len;
 	int i;
@@ -51,39 +51,40 @@ static int		 add_chr(t_info *info, long chr)
 	ret = 0;
 	if (ft_isprint(chr))
 	{
-			cmd_len = ft_strlen(info->term->cmd);
-			i = cmd_len - info->term->pos_c;
-			ret = (i == 0) ? 1 : 2;
-			if (cmd_len < BUFFER_SIZE)
+		cmd_len = ft_strlen(info->term->cmd);
+		i = cmd_len - info->term->pos_c;
+		ret = (i == 0) ? 1 : 2;
+		if (cmd_len < BUFFER_SIZE)
+		{
+			info->term->cmd[cmd_len + 1] = 0;
+			while (i >= 0 && info->term->cmd[info->term->pos_c])
 			{
-					info->term->cmd[cmd_len + 1] = 0;
-					while (i >= 0 && info->term->cmd[info->term->pos_c])
-					{
-							info->term->cmd[info->term->pos_c + i + 1] = info->term->cmd[info->term->pos_c + i];
-							--i;
-					}
-					info->term->cmd[info->term->pos_c] = chr;
-					++info->term->pos_c;
+				info->term->cmd[info->term->pos_c + i + 1] =\
+				info->term->cmd[info->term->pos_c + i];
+				--i;
 			}
+			info->term->cmd[info->term->pos_c] = chr;
+			++info->term->pos_c;
+		}
 	}
 	return (ret);
 }
 
-int		 termcaps_handle_keyboard(t_info *info, long chr)
+int				termcaps_handle_keyboard(t_info *info, long chr)
 {
-		int				 i;
-		int				 ret;
+	int				i;
+	int				ret;
 
-		i = -1;
-		while (++i < 22)
+	i = -1;
+	while (++i < 22)
+	{
+		if (g_key_tab[i].key == chr)
 		{
-			 if (g_key_tab[i].key == chr)
-			 {
-					 g_key_tab[i].f(info);
-					 return (1);
-			 }
+			g_key_tab[i].f(info);
+			return (1);
 		}
-		if (!(ret = add_chr(info, chr)))
-				return (0);
-		return (termcaps_print(info, info->term->cmd, chr));
+	}
+	if (!(ret = add_chr(info, chr)))
+		return (0);
+	return (termcaps_print(info, info->term->cmd, chr));
 }
